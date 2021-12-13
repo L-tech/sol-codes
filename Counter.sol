19d09b80c31470e839bf9e7e686591be9f6e25b4
L-tech <legendabrahamonoja@gmail.com>
2021-12-13 17:50:00 +0100

    first commit

---
+++ Counter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract Counter {
+    uint public count;
+    uint public min_count = 0;
+    uint public max_count = 100;
+
+    // Function to get currenct count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    // function to increment count by 1
+    function inc() public {
+        require(count + 1 <= max_count);
+        count += 1;
+    }
+    // function to increment count by certain number
+    function numInc(uint num) public {
+        require(count + num <= max_count);
+        count += num;
+    }
+    // function to decrement count by 1
+    function dec() public {
+        require(count - 1 >= min_count);
+        count -= 1;
+    }
+    // function to decrement count by a certain number
+    function numDec(uint num) public {
+        require(count - num >= min_count);
+        count -= num;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Sol Codes 
+
+Mini Ethereum Projects built on Solidity.
+
+Each file represent a different projects.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## Contact
+say hello on twitter @arome_dev
