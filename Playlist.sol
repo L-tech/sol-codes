a3951de9076b007538c34f0304be6b0b19766fdf
L-tech <legendabrahamonoja@gmail.com>
2022-02-01 23:56:59 +0100

    update: new playlist functions

---
+++ Playlist.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract Playlist {
+    struct Song {
+        string name;
+        bool favourite;
+    }
+    Song[] public songs;
+
+    function create(string calldata _name) external {
+        songs.push(Song({
+            name: _name,
+            favourite: false
+        }));
+    }
+    function updateSongName(uint _index, string calldata _name) external {
+        songs[_index].name = _name;
+    }
+
+    function getSong(uint _index) view external returns(string memory, bool) {
+        Song storage song = songs[_index];
+        return(song.name, song.favourite);
+    }
+}
